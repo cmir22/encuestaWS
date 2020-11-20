1ac2fb8bc4bb0f66f567a598edd811a8e56895cb
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-11-20 11:32:46 -0800

    Conected to firebase

--- src/app/tests/tests.component.ts
+++ src/app/tests/tests.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 interface CI {
   value: string;
   viewValue: string;
@@ -10,27 +9,44 @@ interface CI {
   templateUrl: './tests.component.html',
   styleUrls: ['./tests.component.css']
 })
+
+
+
+
+
 export class TestsComponent implements OnInit {
 
   cis: CI[] = [
-    {value: 'bajo', viewValue: 'Bajo'},
-    {value: 'alerta', viewValue: 'Alerta'},
-    {value: 'promedio', viewValue: 'Promedio'},
-    {value: 'alto', viewValue: 'Alto'}
+    { value: 'bajo', viewValue: 'Bajo' },
+    { value: 'alerta', viewValue: 'Alerta' },
+    { value: 'promedio', viewValue: 'Promedio' },
+    { value: 'alto', viewValue: 'Alto' }
   ];
 
   abts: CI[] = [
-    {value: '1', viewValue: '1'},
-    {value: '2', viewValue: '2'},
-    {value: '3', viewValue: '3'},
-    {value: '4', viewValue: '4'}
+    { value: '1', viewValue: '1' },
+    { value: '2', viewValue: '2' },
+    { value: '3', viewValue: '3' },
+    { value: '4', viewValue: '4' }
   ];
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
-  }
+    const db = firebase.firestore();
+    const form = document.querySelector("#form")
 
+    const numero  = form.addEventListener('submit',e =>{
+      e.preventDefault();
+      let datos = (<HTMLInputElement>document.querySelector('#datos')).value;
+     db.collection('informacion').doc().set({
+       datos: datos
+     })
+      
+    })
+
+  }
 
 
 }
