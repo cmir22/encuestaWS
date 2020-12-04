6eda0bed638a369329dfaa15dc06be01f40f19ee
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-12-04 09:39:01 -0800

    Updated files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+  production:true,
+     firebaseConfig : {
+      apiKey: "AIzaSyDbi1GXnaP69fZ4fwoAKdiTb0z0MZluhgk",
+      authDomain: "encuestaws.firebaseapp.com",
+      databaseURL: "https://encuestaws.firebaseio.com",
+      projectId: "encuestaws",
+      storageBucket: "encuestaws.appspot.com",
+      messagingSenderId: "276721409501",
+      appId: "1:276721409501:web:bf43783234333fbf562a34",
+      measurementId: "G-ENXF94XSHK"
+    }
+  };
